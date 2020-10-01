742c8cd6d15c785fed6266976b319facfc888509
SixtyHrtz <sixtyhz@pm.me>
2020-10-01 14:17:51 +0300

    Added Trigger

---
+++ ExpEditor.Core/Trigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ExpEditor.Core
+{
+    public class Trigger
+    {
+        public string Name { get; set; }
+
+        public List<IAction> Actions { get; set; }
+
+        public Trigger() => Actions = new List<IAction>();
+
+        public override string ToString() => Name;
+    }
+}
