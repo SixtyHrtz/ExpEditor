3d1c59f2e2460486f4e08f8ead36b3e4f7e8b33a
SixtyHrtz <sixtyhz@pm.me>
2020-10-01 17:15:09 +0300

    Added test If Action

---
+++ ExpEditor.Core/Methods/Actions/IfAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace ExpEditor.Core
+{
+    public class IfAction : Action
+    {
+        public override string Template { get { return "If (All Conditions are True) then do (Then Actions) else do (Else Actions)"; } }
+
+        private readonly List<BooleanExpression> conditions;
+        private readonly List<IAction> thenActions;
+        private readonly List<IAction> elseActions;
+
+        public IfAction()
+        {
+            conditions = new List<BooleanExpression>();
+            thenActions = new List<IAction>();
+            elseActions = new List<IAction>();
+        }
+
+        public override void Execute()
+        {
+            if (!conditions.Contains(false))
+            {
+                foreach (var action in thenActions)
+                    action.Execute();
+            }
+            else
+            {
+                foreach (var action in elseActions)
+                    action.Execute();
+            }
+        }
+    }
+}
